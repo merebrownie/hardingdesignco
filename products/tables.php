55da8bf2ec5f9e8a6b8b261db4cf319b65bf0d39
Meredith Browne <mbrowne2@uncc.edu>
2016-07-10 15:40:52 -0400

    back to woodgrain

--- products/tables.php
+++ products/tables.php
@@ -2,31 +2,31 @@
 <div class="flexslider">
     <ul class="slides">
         <li data-thumb="../img/IMG_3036_thumb.png">
-            <img class="image" src="../img/IMG_3036.jpg" alt="table">
+            <img class="image" src="../img/IMG_3036-min.jpg" alt="table">
             <p class="flex-caption">Description here.</p>
         </li>
         <li data-thumb="../img/IMG_3046_thumb.png">
-            <img class="image" src="../img/IMG_3046.jpg" alt="table">
+            <img class="image" src="../img/IMG_3046-min.png" alt="table">
             <p class="flex-caption">Description here.</p>
         </li>
         <li data-thumb="../img/IMG_3051_thumb.png">
-            <img class="image" src="../img/IMG_3051.jpg" alt="table">
+            <img class="image" src="../img/IMG_3051-min.jpg" alt="table">
             <p class="flex-caption">Description here.</p>
         </li>
         <li data-thumb="../img/IMG_2945_thumb.png">
-            <img class="image" src="../img/IMG_2945.jpg" alt="table">
+            <img class="image" src="../img/IMG_2945-min.jpg" alt="table">
             <p class="flex-caption">Description here.</p>
         </li>
         <li data-thumb="../img/IMG_2950_thumb.png">
-            <img class="image" src="../img/IMG_2950.jpg" alt="table">
+            <img class="image" src="../img/IMG_2950-min.jpg" alt="table">
             <p class="flex-caption">Description here.</p>
         </li>
         <li data-thumb="../img/IMG_2953_thumb.png">
-            <img class="image" src="../img/IMG_2953.jpg" alt="table">
+            <img class="image" src="../img/IMG_2953-min.jpg" alt="table">
             <p class="flex-caption">Description here.</p>
         </li>
         <li data-thumb="../img/IMG_2963_thumb.png">
-            <img class="image" src="../img/IMG_2963.jpg" alt="table">
+            <img class="image" src="../img/IMG_2963-min.jpg" alt="table">
             <p class="flex-caption">Description here.</p>
         </li>
         
